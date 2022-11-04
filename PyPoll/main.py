e46536c04e622cac85bc9ab692ddbae618134373
camillejensen <114604704+camillejensen@users.noreply.github.com>
2022-11-03 19:46:24 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-csvpath = os.path.join("/Users/camillejensen/Desktop/Python-Challenge/PyPoll/Resources/election_data.py")
+csvpath = os.path.join("/Users/camillejensen/Desktop/Python-Challenge/PyPoll/Resources/election_data.csv")
 
 total_votes = 0
 khan_votes = 0
@@ -25,3 +25,22 @@ with open(csvpath,newline="", encoding="utf-8") as elections:
 
 candidates = ["Khan", "Correy", "Li", "O'Tooley"]
 votes = [khan_votes, correy_votes, li_votes, otooley_votes]
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) *100
+li_percent = (li_votes/total_votes) *100
+otooley_percent = (otooley_votes/total_votes) *100
+
+print(f"Election Results")
+print(f"----------------------")
+print(f"Total Votes: {total_votes}")
+print(f"-----------------------")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"----------------------------")
+print(f"Winner: {key}")
+print(f"----------------------------")
