2d3c4795ee459d157dc614731bbe17cb27affc0c
camillejensen <114604704+camillejensen@users.noreply.github.com>
2022-11-04 11:18:59 -0500

    PyPoll code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,9 @@ khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
+stockham_votes = 0
+degette_votes = 0
+doane_votes = 0
 
 with open(csvpath,newline="", encoding="utf-8") as elections:
      csvreader = csv.reader(elections,delimiter=",")
@@ -22,9 +25,17 @@ with open(csvpath,newline="", encoding="utf-8") as elections:
                li_votes +=1
           elif row[2] == "O'Tooley":
                otooley_votes +=1
+          if row[2] == "Charles Casper Stockham":
+               stockham_votes +=1
+          elif row[2] == "Diana DeGette":
+               degette_votes +=1
+          elif row[2] == "Raymon Anthony Doane":
+               doane_votes +=1
 
 candidates = ["Khan", "Correy", "Li", "O'Tooley"]
 votes = [khan_votes, correy_votes, li_votes, otooley_votes]
+candidates = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
+votes = [stockham_votes, degette_votes, doane_votes]
 dict_candidates_and_votes = dict(zip(candidates,votes))
 key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 
@@ -32,6 +43,9 @@ khan_percent = (khan_votes/total_votes) *100
 correy_percent = (correy_votes/total_votes) *100
 li_percent = (li_votes/total_votes) *100
 otooley_percent = (otooley_votes/total_votes) *100
+stockham_percent = (stockham_votes/total_votes) *100
+degette_percent = (degette_votes/total_votes) *100
+doane_percent = (doane_votes/total_votes) *100
 
 print(f"Election Results")
 print(f"----------------------")
@@ -41,6 +55,9 @@ print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
 print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
 print(f"Li: {li_percent:.3f}% ({li_votes})")
 print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"Charles Casper Stockham: {stockham_percent:.3f}% ({stockham_votes})")
+print(f"Diana DeGette: {degette_percent:.3f}% ({degette_votes})")
+print(f"Raymon Anthony Doane: {doane_percent:.3f}% ({doane_votes})")
 print(f"----------------------------")
 print(f"Winner: {key}")
 print(f"----------------------------")
